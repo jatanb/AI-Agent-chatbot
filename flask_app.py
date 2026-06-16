5058435b6c2bd0ad3a8b6e97644aa5aae0f7f91c
jatanb <jatanbhundiya28@gmail.com>
2026-06-16 12:31:39 +0530

    flask add

--- flask_app.py
+++ flask_app.py
@@ -1,29 +1,10 @@
-"""
-Flask web app with multiple pages: Home, Features, Working, Privacy
-"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    """Home page"""
-    return render_template('home.html')
+    return render_template("index.html")
 
-@app.route('/features')
-def features():
-    """Features page"""
-    return render_template('features.html')
-
-@app.route('/working')
-def working():
-    """How it works page"""
-    return render_template('working.html')
-
-@app.route('/privacy')
-def privacy():
-    """Privacy policy page"""
-    return render_template('privacy.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
